17ca870be72ab0100e41f8c696f2c754aa6da451
Matheus-Alpe <matttalves@gmail.com>
2021-05-01 19:50:25 -0300

    doc: removed unnecessary doc

--- src/routes.ts
+++ src/routes.ts
@@ -5,19 +5,6 @@ import { UsersController } from "./controllers/UsersController";
 
 const routes = Router();
 
-/**
- * Tipos de parametros
- * 
- * - Routes Params => Parametros de rotas
- * http://localhost:3333/settings/1
- * 
- * - Query Params => Filtros e buscas
- * http://localhost:3333/settings/1?search=algumacoisa&size=20
- * 
- * - Body Params => Objeto de requisições {
- *      
- *   }
- */
 const settingsController = new SettingsController();
 const usersController = new UsersController();
 const messagesController = new MessagesController();
